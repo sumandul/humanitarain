a2258c291bd8773c1d3d7172757a336cf7fb6c5f
aayushkc <57221458+aayushkc@users.noreply.github.com>
2024-01-29 12:13:17 +0545

    Added Nav Bar

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,8 +1,16 @@
 import Image from "next/image";
-import { Inter } from "next/font/google";
+import hero from "../../public/images/hero.png"
+import { Open_Sans, Roboto } from "next/font/google";
 
-const inter = Inter({ subsets: ["latin"] });
+const openSans = Open_Sans({ subsets: ["latin"] });
+const roboto = Roboto({ subsets: ["latin"], weight: ["400", "700"] });
 
 export default function Home() {
-  return <main>hello world</main>;
+  return (
+    <main>
+        <section className="hero h-[80vh]">
+              www
+        </section>
+    </main>
+  );
 }
